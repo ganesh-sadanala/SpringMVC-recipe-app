150cc22e9d995dfcad0b2bb2629b13ad6764b8d2
gsadanala <gsadanala@paypal.com>
2021-04-19 12:07:06 +0530

    Create One to One JPA Relationship between Recipe and Notes Entities

--- src/main/java/com/ganesh/recipeapp/RecipeAppApplication.java
+++ src/main/java/com/ganesh/recipeapp/RecipeAppApplication.java
@@ -8,6 +8,6 @@ public class RecipeAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeAppApplication.class, args);
+		System.out.println("Hello World");
 	}
-
 }
