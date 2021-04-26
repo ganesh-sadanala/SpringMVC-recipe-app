584aa3396b62528d74d35b231bf7a3f537b92d68
gsadanala <gsadanala@paypal.com>
2021-04-26 11:53:30 +0530

    Database initialization using spring Data Initializer via spring boot using data.sql file

--- src/main/java/com/ganesh/recipeapp/repositories/CategoryRepository.java
+++ src/main/java/com/ganesh/recipeapp/repositories/CategoryRepository.java
@@ -4,4 +4,5 @@ import com.ganesh.recipeapp.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    
 }
