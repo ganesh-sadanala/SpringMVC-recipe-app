21835d1f427e91b373ad701cf5f1b09c9f7806f6
gsadanala <gsadanala@paypal.com>
2021-05-15 15:30:13 +0530

    Using Mockito mocks

---
+++ src/test/java/com/ganesh/recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.ganesh.recipeapp.services;
+
+import com.ganesh.recipeapp.domain.Recipe;
+import com.ganesh.recipeapp.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    RecipeService recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipesData);
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(2)).findAll();
+    }
+}
