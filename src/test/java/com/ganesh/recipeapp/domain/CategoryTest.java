87aa24693ed7c1c7985f8624f5ba80bcae2ce2d0
gsadanala <gsadanala@paypal.com>
2021-05-15 12:49:02 +0530

    Create a Category unit test

---
+++ src/test/java/com/ganesh/recipeapp/domain/CategoryTest.java
@@ -0,0 +1,30 @@
+package com.ganesh.recipeapp.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CategoryTest {
+
+    Category category;
+
+    @BeforeEach
+    public void setUp(){
+        category = new Category();
+    }
+
+    @Test
+    void getId() {
+        Long idValue = 4l;
+        category.setId(idValue);
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getRecipes() {
+    }
+}
